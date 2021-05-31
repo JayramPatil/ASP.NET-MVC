bf7fbd8e7c6c6c66bc80bac8efa65663a22b3781
Jayram <Jayram1234patil@gmail.com>
2021-05-31 18:38:58 +0530

    adds filter to index method

---
+++ MVCFiltersDemo/MVCFiltersDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFiltersDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        [OutputCache( Duration = 15 )]
+        public string Index()
+        {
+            return "Hiiii";
+        }
+        [OutputCache(Duration = 15)]
+        public string DisplayDate()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
---
+++ MVCFiltersDemo/MVCFiltersDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCFiltersDemo.MvcApplication" Language="C#" %>
